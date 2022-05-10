7d859251b9a200a54690ef3ac024eec145b43ad3
amdadul-haque <amdadul1807107@gmail.com>
2022-05-10 06:47:19 +0600

    editing b1 from b2

--- src/firstcalculator/b1.java
+++ src/firstcalculator/b1.java
@@ -2,13 +2,24 @@ package firstcalculator;
 
 public class b1 {
     int income;
+    int cgpa;
+
+    public int getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(int cgpa) {
+        this.cgpa = cgpa;
+    }
+
 
     public int getIncome() {
         return income;
     }
 
-    public void setIncome(int income) {
+    public void setIncome(int income,int cgpa) {
         this.income = income;
+        this.cgpa = cgpa;
     }
 
     public b1(int income) {
